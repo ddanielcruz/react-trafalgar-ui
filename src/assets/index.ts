0a713f7e4d27a3918d661a2ce20ffaaf838fa289
Daniel Cruz <danielcunha54@gmail.com>
2023-02-21 13:06:30 -0300

    style: add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,25 @@
+import cloud from './cloud.png'
+import hero from './hero.png'
+import landingApps from './landing-apps.png'
+import landingProviders from './landing-providers.png'
+import service1 from './service-1.svg'
+import service2 from './service-2.svg'
+import service3 from './service-3.svg'
+import service4 from './service-4.svg'
+import service5 from './service-5.svg'
+import service6 from './service-6.svg'
+import squareDots from './square-dots.svg'
+
+export {
+  cloud,
+  hero,
+  landingApps,
+  landingProviders,
+  service1,
+  service2,
+  service3,
+  service4,
+  service5,
+  service6,
+  squareDots
+}
